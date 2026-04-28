811f7c01d6fc5db3bcf67ff9e76e1cab611aa527
M-Brown26 <crashcontroldev@gmail.com>
2026-04-28 15:37:11 -0400

    updated treaties queries

--- database/treatiesQueries.sql
+++ database/treatiesQueries.sql
@@ -11,3 +11,9 @@ VALUES
 ('Outer Space Treaty', 'Algeria', '1992-01-27', 1992),
 ('Outer Space Treaty', 'China', '1983-12-30', 1983),
 ('Outer Space Treaty', 'United States of America', '1967-10-10', 1967);
+
+-- Countries who signed given treaty
+SELECT country_name FROM Signatures WHERE treaty_title = 'Moon Treaty';
+
+-- Treaty signed by given country
+SELECT treaty_title FROM Treaties WHERE country_name = 'China';
